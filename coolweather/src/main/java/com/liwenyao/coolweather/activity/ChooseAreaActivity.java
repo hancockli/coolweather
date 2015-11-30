2e453043f76ed714c60d8b027b5b218c5298f678
Hancock <lihancock7@gmail.com>
2015-11-30 17:30:17 +0800

    forth commit

--- coolweather/src/main/java/com/liwenyao/coolweather/activity/ChooseAreaActivity.java
+++ coolweather/src/main/java/com/liwenyao/coolweather/activity/ChooseAreaActivity.java
@@ -2,7 +2,10 @@ package com.liwenyao.coolweather.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -30,7 +33,7 @@ import java.util.List;
 public class ChooseAreaActivity extends Activity {
     public static final int LEVEL_PROVINCE = 0;
     public static final int LEVEL_CITY = 1;
-    public static final int LECEL_COUNTY = 2;
+    public static final int LEVEL_COUNTY = 2;
 
     private ProgressDialog progressDialog;
     private TextView titleText;
@@ -48,6 +51,13 @@ public class ChooseAreaActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(prefs.getBoolean("city_selected", false)) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
         listView = (ListView) findViewById(R.id.list_view);
@@ -61,10 +71,15 @@ public class ChooseAreaActivity extends Activity {
                 if (currentLevel == LEVEL_PROVINCE) {
                     selectedProvince = provinceList.get(position);
                     queryCities();
-                }
-                if (currentLevel == LEVEL_CITY) {
+                } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
+                } else if (currentLevel == LEVEL_COUNTY) {
+                    String countyCode = countyList.get(position).getCountyCode();
+                    Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+                    intent.putExtra("county_code", countyCode);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
@@ -184,7 +199,7 @@ public class ChooseAreaActivity extends Activity {
     }
 
     public void onBackPressed() {
-        if (currentLevel == LECEL_COUNTY) {
+        if (currentLevel == LEVEL_COUNTY) {
             queryCities();
         } else if (currentLevel == LEVEL_CITY) {
             queryProvinces();
--- coolweather/src/main/java/com/liwenyao/coolweather/db/CoolWeatherOpenHelper.java
+++ coolweather/src/main/java/com/liwenyao/coolweather/db/CoolWeatherOpenHelper.java
@@ -19,7 +19,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
     /**
      * County表建表语句
      */
-    public static final String CREATTE_COUNTY = "create table County(id integer primary key autoincrement, county_name text, county_name text, city_id integer)";
+    public static final String CREATTE_COUNTY = "create table County(id integer primary key autoincrement, county_name text, county_code text, city_id integer)";
 
     public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
